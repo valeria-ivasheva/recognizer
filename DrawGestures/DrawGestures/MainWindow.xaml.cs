0c07bd6fa9851940cd1d3ab9d6b8ed19c09ac95e
valeria-ivasheva <valeria.ivasheva@outlook.com>
2019-03-04 14:45:39 +0300

    Форма для сбора базы жестов для тестирования

---
+++ DrawGestures/DrawGestures/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrawGestures
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var mainViewModel = new MainViewModel(this);
+            DataContext = mainViewModel;
+        }
+
+        private void inkCanvasMouseMove(object sender, MouseEventArgs e)
+        {
+            (DataContext as MainViewModel).MouseMove(sender, e);
+        }
+
+        private void inkCanvasPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            (DataContext as MainViewModel).PreviewMouseLeftButtonDown(sender, e);
+        }
+
+        private void inkCanvasPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            (DataContext as MainViewModel).PreviewMouseLeftButtonUp();
+        }
+
+        private void ComboBoxSelected(object sender, SelectionChangedEventArgs e)
+        {
+            (DataContext as MainViewModel).ComboBoxSelected(sender, e);
+        }
+
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            (DataContext as MainViewModel).BeforeClose();
+        }
+    }
+}
---
+++ DrawGestures/DrawGestures/UsersGestures.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DrawGestures
+{
+    //class UsersGestures
+    //{
+    //}
+    [Serializable()]
+    public class UserPath
+    {
+        [XmlAttribute("path")]
+        public string Path { get; set; }
+
+        public UserPath()
+        {
+        }
+
+        public UserPath(string path)
+        {
+            Path = path;
+        }
+    }
+
+    [Serializable()]
+    public class Gestures
+    {
+        [XmlAttribute("idealPath")]
+        public string IdealPath { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlArray("UserPaths")]
+        [XmlArrayItem("UserPath", typeof(UserPath))]
+        public List<UserPath> UserPath { get; set; }
+
+        public Gestures()
+        {
+            UserPath = new List<UserPath>();
+        }
+    }
+
+    [Serializable()]
+    [XmlRoot("GesturesCollection")]
+    public class GesturesCollection
+    {
+        [XmlArray("UsersGestures")]
+        [XmlArrayItem("Gesture", typeof(Gestures))]
+        public List<Gestures> Gesture { get; set; }
+
+        public GesturesCollection()
+        {
+            Gesture = new List<Gestures>();
+        }
+    }
+}
